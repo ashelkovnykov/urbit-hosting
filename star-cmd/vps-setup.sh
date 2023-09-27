065d037cb84c85829b74a6fb875751cb6549ceef
tomholford <tomholford@users.noreply.github.com>
2023-09-26 23:10:33 -0700

    deps: update docker-compose to latest

--- star-cmd/vps-setup.sh
+++ star-cmd/vps-setup.sh
@@ -63,7 +63,7 @@ apt update
 apt install docker-ce
 
 # install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/v2.14.2/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.22.0/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # add system user to sudo group
